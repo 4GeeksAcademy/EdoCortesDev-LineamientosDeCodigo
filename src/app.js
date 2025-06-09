dec4034527ac6572351faca8f763ecb9d3ccc80c
edoCortesDev <edo.cortes.rojas@gmail.com>
2025-06-09 16:46:27 +0000

    actualizacion codigo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  generarDominios();
 };
+
+function generarDominios() {
+  const pronombres = ["el", "ese", "aquel"];
+  const adjetivos = ["valiente", "inteligente", "fuerte"];
+  const sustantivos = ["guerrero", "científico", "atleta"];
+
+  const dominios = combinarPalabras(pronombres, adjetivos, sustantivos);
+  mostrarDominios(dominios);
+}
+
+function combinarPalabras(pronombres, adjetivos, sustantivos) {
+  const resultados = [];
+  
+  for (const pronombre of pronombres) {
+    for (const adjetivo of adjetivos) {
+      for (const sustantivo of sustantivos) {
+        resultados.push(`${pronombre}${adjetivo}${sustantivo}.com`);
+      }
+    }
+  }
+  
+  return resultados;
+}
+
+function mostrarDominios(lista) {
+  for (const dominio of lista) {
+    console.log(dominio);
+  }
+}
